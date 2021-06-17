76729650cc4b4cda162dea41794b8e88cdae6448
shivaramh <no123@MEC>
2021-06-17 17:59:38 +0530

    Skelton FrameWork

--- src/test/java/org/HomePageTest.java
+++ src/test/java/org/HomePageTest.java
@@ -1,4 +1,13 @@
 package org;
 
-public class HomePageTest {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import resources.BaseClass;
+
+public class HomePageTest extends BaseClass {
+    public WebDriver driver;
+
+    public static Logger log = LogManager.getLogger(HomePageTest.class.getName());
+
 }
